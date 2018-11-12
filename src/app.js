0c44f6fee46a3471dc2563b85ab0983e52f00d54
Koji Hatanaka <kojiha@users.noreply.github.com>
2018-11-13 02:35:45 +0900

    intermediate checkins

--- src/app.js
+++ src/app.js
@@ -9,10 +9,20 @@ const port = process.env.PORT || 8081;
 // Kubernetes Client
 const k8s = require('@kubernetes/client-node');
 const k8sApiEndpoint = '/api/v1/watch/namespaces/showks/services';
-//const k8sApiEndpoint = '/apis/apps/v1/watch/namespaces/showks/deployments';
+const kc = new k8s.KubeConfig();
+kc.loadFromCluster();
+
 
 // Instances
 let instances = {};
+
+// Helper functions
+function getServiceUrl(instance) {
+  let host = obj.spec.clusterIP;
+  let port = obj.spec.ports[0].port;
+  return `http://${host}:${port}`;
+}
+
 
 // Setup the express web app
 
@@ -31,12 +41,23 @@ app.get('/:id/thumbnail', function (req, res) {
 
 // GET /author
 app.get('/:id/author', function (req, res) {
-  res.type("json");
-  res.send(JSON.stringify({
-    name: "Koji Hatanaka",
-    twitter: "@kojiha__",
-    note: "This endpoint is to be implemented"
-  }));
+  let url;
+  try {
+    let instance = instances[id];
+    url = getServiceUrl(instance);
+  } catch (err) {
+    console.log('an error occurred on getting instance in /:id/author');
+    console.log(err);
+  }
+  got(`http://${host}:${port}`, {})
+  .then(response => {
+    console.log('responce: ' + response.body);
+    res.type("json");
+    res.send(response.body);
+  }).catch(error => {
+    console.log('an error occurred on fetching author in /:id/author');
+    console.log(error);
+  });
 })
 
 // socket.io connection handler
@@ -55,41 +76,35 @@ instanceNamespace.on('connection', onInstanceConnection);
 http.listen(port, () => console.log('listening on port ' + port));
 
 // Start watching Kubernetes cluster
-const kc = new k8s.KubeConfig();
-kc.loadFromCluster();
-
 let watch = new k8s.Watch(kc);
 let req = watch.watch(
   k8sApiEndpoint,
-  {},
+  {
+    labelSelector: 'app=canvas'
+  },
   (type, obj) => {
-    if (type == 'ADDED') {
-      console.log('new object:');
-      instances[obj.metadata.name] = obj;
-//      commandNamespace.emit('updated', obj.metadata.name);
-    } else if (type == 'MODIFIED') {
-      console.log('changed object:')
-      instances[obj.metadata.name] = obj;
-//      commandNamespace.emit('updated', obj.metadata.name);
-    } else if (type == 'DELETED') {
-      console.log('deleted object:');
-      delete instances[obj.metadata.name];
-//      commandNamespace.emit('deleted', obj.metadata.name);
-    } else {
-      console.log('unknown type: ' + type);
+    try {
+      if (type == 'ADDED' || type == 'MODIFIED') {
+        console.log('added or modified object:');
+        instances[obj.metadata.name] = obj;
+  //      commandNamespace.emit('updated', obj.metadata.name);
+      } else if (type == 'DELETED') {
+        console.log('deleted object:');
+        delete instances[obj.metadata.name];
+  //      commandNamespace.emit('deleted', obj.metadata.name);
+      } else {
+        console.log('unknown type: ' + type);
+      }
+      console.log(obj);  
+    } catch (err) {
+      console.log('an error occurred on parsing service object');
+      console.log(err);
     }
-    console.log(obj);
-    let host = obj.spec.clusterIP;
-    let port = obj.spec.ports[0].port;
-    got(`http://${host}:${port}`, {})
-    .then(response => {
-      console.log('responce: ' + response.body);
-    }).catch(error => {
-    });
   },
   // done callback is called if the watch terminates normally
-  (err) => {
-      if (err) {
-          console.log(err);
+  (error) => {
+      if (error) {
+        console.log('an error occurred in the watch callback');
+        console.log(error);
       }
   });
